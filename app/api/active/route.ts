c07799a728653920d836814fe62252fb69616094
aqua <84078890+phthallo@users.noreply.github.com>
2025-04-09 10:18:32 -0400

    add back button

--- app/api/active/route.ts
+++ app/api/active/route.ts
@@ -24,43 +24,52 @@ export async function GET(request: NextRequest){
 }
 
 export async function POST(request: NextRequest){
+   
+    const back = request.nextUrl.searchParams.get("back")
+    console.log(back)
     const totalSlides = (await prisma.slide.findMany()).length
-
-    const activeSlide = await prisma.slide.findFirst({
-        where: {
-            current: true
-        }, 
-        select: {
-            id: true
-        }
-        })
-    const deactivateSlide = await prisma.slide.update({
-        where: {
-            id: activeSlide!.id
-        },
-        data: {
-            current: false
-        }
+    let activeSlide = await prisma.slide.findFirst({
+            where: {
+                current: true
+            }, 
+            select: {
+                id: true
+            }
     })
-
-    //try { 
-        const activateNext = await prisma.slide.update({
-        where: {
-            id: wrap(activeSlide!.id, totalSlides)
-        },
-        data: {
-            current: true
+        if (!activeSlide){
+            activeSlide = { id: 1 }
         }
-        }) 
-    //} catch { 
-    //    const activateFirst = await prisma.slide.update({
-    //        where: {
-    //            id: 1
-    //        },
-    //        data: { 
-    //            current: true
-    //        }
-    //    })
-    //}
-    return NextResponse.json(activeSlide)
+        if (back){
+            console.log('back true', activeSlide, activeSlide.id-1)
+            if (activeSlide!.id === 1){
+                return 
+            } else {
+                const activatePrev = await prisma.slide.update({
+                    where: {
+                        id: activeSlide!.id - 1
+                    },
+                    data: {
+                        current: true
+                    }
+                    }) 
+            }
+        } else {
+            const activateNext = await prisma.slide.update({
+                where: {
+                    id: wrap(activeSlide!.id, totalSlides)
+                },
+                data: {
+                    current: true
+                }
+                }) 
+        }
+        const deactivateSlide = await prisma.slide.update({
+            where: {
+                id: activeSlide!.id
+            },
+            data: {
+                current: false
+            }
+        })
+        return NextResponse.json(activeSlide)
 }
--- app/components/Navigate.tsx
+++ app/components/Navigate.tsx
@@ -6,24 +6,41 @@ export default function Navigate({currentSlide, setCurrentSlide, className}: {cu
     const session = useSession();
 
 
-    async function handleClick(){
+
+    async function handleForward(){
         const totalSlides = (await fetch("/api/slides").then(r => r.json()))["count"]
         setCurrentSlide(wrap(currentSlide, totalSlides))
-
-    }
+        }
+    
+        async function handleBack(){
+            if (currentSlide == 1){
+                // do nothing
+            } else {
+                setCurrentSlide(currentSlide-1)
+            }
+        }
 
     if (session.status === "authenticated" && (session.data?.user! as NewNextAuthUser).username === process.env.NEXT_PUBLIC_AUTHORISED_USER){
         return (
-            <button className = {className} onClick={async () => {handleClick(); await fetch("/api/active", { method: "POST" })}}>
+            <div className = "flex flex-row gap-4">
+                <button className = {className} onClick={async () => {handleBack(); await fetch("/api/active?back=true", { method: "POST" })}}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
                 </svg>
-                next (admin)
-            </button>
+                    back (admin)
+                </button>
+                | 
+                <button className = {className} onClick={async () => {handleForward(); await fetch("/api/active", { method: "POST" })}}>
+                    next (admin)
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                    </svg>
+                </button>
+            </div>
         )
     } else {
         return (
-            <button className = {className}  onClick={handleClick}>
+            <button className = {className}  onClick={handleForward}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
                 </svg>
